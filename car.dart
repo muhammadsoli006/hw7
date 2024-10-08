645c306ad1bc179300849afdc17291e8b655745b
unknown <saidkosievmuhammad@gmail.com>
2024-10-08 17:28:02 +0600

    first

---
+++ car.dart
@@ -0,0 +1,24 @@
+
+class Car {
+  String model;
+  int year;
+  List<Engine> engine;
+  Car({
+    required this.model,
+    required this.year,
+    required this. engine,
+  });
+
+}
+
+
+
+ 
+class Engine {
+  int abyom;
+  int Liter;
+  Engine({
+    required this.abyom,
+    required this.Liter,
+  });
+ }
